76e46f3213c85eb794b15f03fcbc064ab20efc34
bentevissel <univ5946@ox.ac.uk>
2025-10-28 15:55:02 +0000

    separated the functions into a functions.py file to make more modular

---
+++ scripts/functions.py
@@ -0,0 +1,20 @@
+#functions used in calcu.ipynb
+import numpy as np
+from math import *
+
+def haversine(lat1, lat2, lon1, lon2):
+    #convert decimal degrees to radians
+    lon1, lat1, lon2, lat2 = map(radians, [lon1, lat1, lon2, lat2])
+
+    #haversine formula
+    dlon = lon2 - lon1 
+    dlat = lat2 - lat1 
+    a = sin(dlat/2)**2 + cos(lat1) * cos(lat2) * sin(dlon/2)**2
+    c = 2 * asin(sqrt(a)) 
+    r = 6371 # Radius of earth in kilometers. Use 3956 for miles. Determines return value units.
+    base = c * r
+    return base
+
+
+def mean_rainfall(data):
+    return np.mean(data)
