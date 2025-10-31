7631569c1bb04609a62452e39e9119bda92c1dec
jessicacbm <148397027+jessicacbm@users.noreply.github.com>
2025-10-31 10:06:16 +0000

    Delete airtools-v2 directory
    deleting airtools-v2

--- airtools-v2/__init__.py
+++
@@ -1,6 +0,0 @@
-#any object defined in this file is available under the packages namespace
-
-from .functions import mean_rainfall, haversine
-from .calcu import TrajectoryCalculations
-
-__all__ = ["mean_rainfall", "haversine", "TrajectoryCalculations"]
