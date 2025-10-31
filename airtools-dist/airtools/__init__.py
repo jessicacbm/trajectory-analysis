4316341d32dd653e824d4f9c987fb8e396ad0abf
bentevissel <univ5946@ox.ac.uk>
2025-10-31 11:43:52 +0000

    updated the init file in the package to include all files

--- airtools-dist/airtools/__init__.py
+++ airtools-dist/airtools/__init__.py
@@ -2,5 +2,8 @@
 
 from .functions import mean_rainfall, haversine
 from .calcu import TrajectoryCalculations
+from .format_visualise_traj import Traj
+from .run_pysplit import Trajectory, Inputs
 
-__all__ = ["mean_rainfall", "haversine", "TrajectoryCalculations"]
+__all__ = ["mean_rainfall", "haversine", "TrajectoryCalculations", "Traj", "Trajectory", "Inputs"]
+           
