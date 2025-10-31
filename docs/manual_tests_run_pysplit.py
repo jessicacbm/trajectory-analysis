3725fe468258bc612eb50f786c4c78e4ad92028c
bentevissel <univ5946@ox.ac.uk>
2025-10-31 16:00:47 +0000

    cleaned up repo

--- docs/manual_tests_run_pysplit.py
+++
@@ -1,4 +0,0 @@
-import format_visualise_traj as pkg
-
-traj = pkg.Traj("/home/jessicbm/trajectory-analysis/.gitignore/example_traj_file.txt")
-plot = traj.plot_traj("traj plot", "precip", extent=[-80, -60, 35, 50])
---
+++ example_docs/example_code.py
@@ -0,0 +1,14 @@
+from airtools as pkg
+
+
+traj = format_visualise_traj.Traj('/home/jessicbm/trajectory-analysis/docs/example_traj_file.txt')
+
+traj.plot_traj("test", "pressure", extent=[-180,180,-90,90])
+
+traj.show()
+
+traj.distance_travelled()
+
+traj.boundary_position()
+
+traj.rainfall_stats()
